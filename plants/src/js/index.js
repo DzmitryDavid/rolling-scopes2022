a0f4f5db1160b7e1bfa63e11f04eb03ce54ce955
dzmitrdavid <Dzmitrydavidovich89@gmail.com>
2023-01-06 20:36:21 +0100

    feat: implement burger functionality

--- plants/src/js/index.js
+++ plants/src/js/index.js
@@ -1,3 +1,15 @@
 console.log(
   '1.Вёрстка валидная +10\n 2.Вёрстка семантическая +20\n 3.Вёрстка соответствует макету +48 \n 4.Требования к css + 12\n 5.Интерактивность, реализуемая через css +20\n Итого 110 баллов'
 );
+
+const burgerEl = document.querySelector('.hamburger');
+const menuEl = document.querySelector('.menu');
+const menuCloseEl = document.querySelector('.menu__close');
+const openMenu = () => {
+  menuEl.classList.add('active');
+}
+const hideMenu = () => {
+  menuEl.classList.remove('active');
+}
+burgerEl.addEventListener('click', openMenu);
+menuCloseEl.addEventListener('click', hideMenu);
