7a91645317842163c6cd9d97bd19be84f8c8f9d3
dzmitrdavid <Dzmitrydavidovich89@gmail.com>
2023-01-28 19:24:07 +0100

    fix: fix deploy

--- plants/src/js/index.js
+++ plants/src/js/index.js
@@ -7,4 +7,4 @@ accordion();
 select();
 filter();
 burger()
-console.log('1.При нажатии на кнопки:Gargens,Lawn,Planting происходит смена фокуса на услугах в разделе service +50\n2.Accordion в секции prices реализация 3-х выпадающих списков об услугах и ценах + 50\n3.В разделе contacts реализован select с выбором городов +25' );
+console.log('1.При нажатии на кнопки:Gargens,Lawn,Planting происходит смена фокуса на услугах в разделе service +50\n2.Accordion в секции prices реализация 3-х выпадающих списков об услугах и ценах + 50\n3.В разделе contacts реализован select с выбором городов +25');
